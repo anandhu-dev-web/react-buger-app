9fc05302020c6512e7eb925937d385c1db33484e
Anandhu K <anandhu.x90@gmail.com>
2025-04-13 09:26:13 +0530

    added about section in home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Layout from '../../components/Layouts/Layout';
 import Section1 from './Section1';
 import "../../styles/HomeStyle.css";
+import Section2 from './Section2';
+import Section3 from './Section3';
 
 function Home() {
   return (
@@ -9,6 +11,10 @@ function Home() {
     <Layout>
       {/* {Home Section Hero Banner} */
       <Section1/>}
+      {/* Home Section About  */}
+      <Section2/>
+      {/* Home Section Menu */}
+      <Section3/>
     </Layout>
     </>
   )
